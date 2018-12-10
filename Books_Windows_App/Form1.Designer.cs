1f2d410f477db5c24ff53c07ed3bd0e91e4282fe
Sauhard <samsauhard1@gmail.com>
2018-12-10 18:41:25 -0500

    Initial commit

---
+++ Books_Windows_App/Form1.Designer.cs
@@ -0,0 +1,84 @@
+﻿namespace Books_Windows_App
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.authorBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.outputTextBox = new System.Windows.Forms.TextBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.authorBindingSource)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // authorBindingSource
+            // 
+            this.authorBindingSource.DataSource = typeof(Books_Lib.Author);
+            // 
+            // outputTextBox
+            // 
+            this.outputTextBox.Location = new System.Drawing.Point(2, 2);
+            this.outputTextBox.Multiline = true;
+            this.outputTextBox.Name = "outputTextBox";
+            this.outputTextBox.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.outputTextBox.Size = new System.Drawing.Size(444, 310);
+            this.outputTextBox.TabIndex = 0;
+            this.outputTextBox.WordWrap = false;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(469, 2);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.textBox1.Size = new System.Drawing.Size(503, 310);
+            this.textBox1.TabIndex = 1;
+            this.textBox1.WordWrap = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(994, 314);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.outputTextBox);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.authorBindingSource)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.BindingSource authorBindingSource;
+        private System.Windows.Forms.TextBox outputTextBox;
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
+
---
+++ Books_Windows_App/Form1.cs
@@ -0,0 +1,78 @@
+﻿using Books_Lib;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.Entity;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Books_Windows_App
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Books_Lib.BooksEntities dbC = new Books_Lib.BooksEntities();
+            dbC.Titles.Load();
+
+
+
+            var authorsAndTitles = from book in dbC.Titles
+                                   orderby book.Title1
+                                   select new { book.Title1, book.Authors };
+
+            outputTextBox.AppendText("\r\n\r\n\t Authors \t\t\t Titles:");
+            foreach (var item in authorsAndTitles)
+            {
+                var authorss = item.Authors;
+                foreach(var item1 in authorss)
+                {
+                    outputTextBox.AppendText(string.Format("\r\n\t{0,-10} {1,-10} {2}", item1.LastName, item1.FirstName, item.Title1));
+                }
+
+               
+            }
+
+
+
+
+
+
+            var authorsAndTitles1 = from book in dbC.Titles
+                                   orderby book.Title1
+                                   select new { book.Title1, book.Authors };
+
+            textBox1.AppendText("\r\n\r\n\t Authors \t\t\t Titles:");
+            foreach (var item in authorsAndTitles1)
+            {
+                var authorss = from author in item.Authors
+                               orderby author.LastName,author.FirstName
+                               select author;
+                foreach (var item1 in authorss)
+                {
+                    textBox1.AppendText(string.Format("\r\n\t{0,-10} {1,-10} {2}", item1.LastName, item1.FirstName, item.Title1));
+                }
+
+
+            }
+
+
+
+
+
+            
+          
+
+
+        }
+    }
+}
